65e16c7c6612623c9f7afba9d3225d6a5268f91d
Tathev Dilanyan <119684982+tathev3@users.noreply.github.com>
2024-04-24 17:01:30 +0400

    vector3.cpp

--- homework_vector/vector3.cpp
+++ homework_vector/vector3.cpp
@@ -2,113 +2,95 @@
 #include <cmath>
 
 class Vector3 {
-    public:
-        double x,y,z;
+public:
+    double x, y, z;
 
-        friend std::ostream& operator<< (  std::ostream& os, const Vector3& v);
-        friend std::istream& operator>> ( std::istream& is,  Vector3& v);
+    Vector3() : x(0), y(0), z(0) {} 
+    Vector3(double x, double y, double z) : x(x), y(y), z(z) {}
 
-        Vector3 operator+ (const Vector3 &V1 );
-        Vector3 operator- (const Vector3& V1);
-        bool operator== (const Vector3 &V1);
-        Vector3 operator= (const Vector3 &V1);
-        Vector3& operator++ ();
-        Vector3& operator++(int);
-        Vector3 operator*(int) const;
-        Vector3 operator/(int) const;
+    friend std::ostream& operator<<(std::ostream& os, const Vector3& v);
+    friend std::istream& operator>>(std::istream& is, Vector3& v);
 
-        double vectors_lenght() const;
-        
+    Vector3 operator+(const Vector3& V1) const;
+    Vector3 operator-(const Vector3& V1) const;
+    Vector3 operator*(double n) const;
+    Vector3 operator/(double n) const;
+
+    bool operator==(const Vector3& V1) const;
+
+    Vector3& operator=(const Vector3& V1);
+
+    Vector3& operator++();  
+    Vector3 operator++(int); 
+
+    double vectors_length() const;
 };
 
-
-int main(){
-    Vector3 v1;
-    std::cin>>v1;
-    
-    double l = v1.vectors_lenght();
-
-    Vector3 v = v1/5;
-    
-    return 0;
+std::ostream& operator<<(std::ostream& os, const Vector3& v) {
+    os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+    return os;
 }
 
- Vector3 Vector3::operator+ (const Vector3 &V1){
-    Vector3 v;
-    v.x = V1.x + this->x;
-    v.y = V1.y + this->y;
-    v.z = V1.z + this->z;
-    return v;
-}
-
-std::ostream& operator<< (  std::ostream& os, const Vector3& v){
-     os <<"("<< v.x <<","<< v.y <<","<< v.z <<")"<<"\n";  
-     return os;
-}
-
-std::istream& operator>> ( std::istream& is, Vector3& v){
-    std::cout << " Enter x,y,z coordinates:: ";
+std::istream& operator>>(std::istream& is, Vector3& v) {
     is >> v.x >> v.y >> v.z;
     return is;
 }
 
-Vector3 Vector3::operator- (const Vector3& V1){
-    Vector3 v;
-    v.x = this->x - V1.x;
-    v.y = this->y - V1.y;
-    v.z = this->z - V1.z;
-    return v;
+Vector3 Vector3::operator+(const Vector3& V1) const {
+    return Vector3(x + V1.x, y + V1.y, z + V1.z);
 }
 
-bool Vector3::operator== (const Vector3& V1){
-    Vector3 v;
-    if (V1.x==this->x && V1.y==this->y && V1.z==this->z)
-        return true;
-    else   
-        return false;
+Vector3 Vector3::operator-(const Vector3& V1) const {
+    return Vector3(x - V1.x, y - V1.y, z - V1.z);
 }
 
-Vector3 Vector3::operator= (const Vector3 &V1){
-    Vector3 v;
-    v.x=V1.x;
-    v.y=V1.y;
-    v.z=V1.z;
-    return v;
+bool Vector3::operator==(const Vector3& V1) const {
+    return (x == V1.x && y == V1.y && z == V1.z);
 }
 
-Vector3& Vector3::operator++ (){
-    x++;
-    y++;
-    z++;
+Vector3& Vector3::operator=(const Vector3& V1) {
+    if (this != &V1) {
+        x = V1.x;
+        y = V1.y;
+        z = V1.z;
+    }
     return *this;
 }
 
-Vector3& Vector3::operator++(int){
-    Vector3 v =*this;
+Vector3& Vector3::operator++() {
+    ++x; ++y; ++z;
+    return *this;
+}
+
+Vector3 Vector3::operator++(int) {
+    Vector3 temp = *this;
     ++*this;
-    return *this; 
-
+    return temp;
 }
 
-Vector3 Vector3::operator*(int n) const {
-    Vector3 v;
-    v.x=x*n;
-    v.y=y*n;
-    v.z=z*n;
-    return v;
+Vector3 Vector3::operator*(double n) const {
+    return Vector3(x * n, y * n, z * n);
 }
 
-
-Vector3 Vector3::operator/(int n) const {
-    Vector3 v;
-    v.x=x/n;
-    v.y=y/n;
-    v.z=z/n;
-    return v;
+Vector3 Vector3::operator/(double n) const {
+    if (n == 0) {
+        throw std::runtime_error("Division by zero exception");
+    }
+    return Vector3(x / n, y / n, z / n);
 }
 
-double Vector3::vectors_lenght() const {
-    double n;
-    n=sqrt(x*x+y*y+z*z);
-    return n;
+double Vector3::vectors_length() const {
+    return sqrt(x * x + y * y + z * z);
 }
+
+int main() {
+    Vector3 v1;
+    std::cin >> v1;
+    std::cout << "Vector: " << v1 << "\n";
+    double length = v1.vectors_length();
+    std::cout << "Length: " << length << "\n";
+    Vector3 v2 = v1 / 5;
+    std::cout << "Scaled vector: " << v2 << "\n";
+    return 0;
+}
+
