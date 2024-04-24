d3e9bb06c17cce2e69a1dbb56468b35a33db3312
Tathev Dilanyan <119684982+tathev3@users.noreply.github.com>
2024-04-24 17:06:57 +0400

    DS.cpp

--- homework_2/DS.cpp
+++ homework_2/DS.cpp
@@ -1,7 +1,5 @@
 int hourglassSum(vector<vector<int>> arr) {
-    int maxSum = arr[0][0] + arr[0][1] + arr[0][2] +
-                 arr[1][1] +
-                 arr[2][0] + arr[2][1] + arr[2][2];
+    int maxSum = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2];
 
     for (int i = 0; i <= 3; i++) {
         for (int j = 0; j <= 3; j++) {
@@ -9,5 +7,6 @@ int hourglassSum(vector<vector<int>> arr) {
             maxSum = max(maxSum, sum);
         }
     }
+    
     return maxSum;
 }
